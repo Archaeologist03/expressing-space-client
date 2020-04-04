f1adfebca8abe031ca102ddfba1faf1a9a409537
Archaeologist03 <omniiknight@hotmail.com>
2020-04-04 18:08:28 +0200

    Adding basic prettier setup

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import styled from 'styled-components';
 import './App.module.scss';
 
 function App() {
-	return (
-		<div className='App'>
-			<Title>Hello there!</Title>
-		</div>
-	);
+  return (
+    <div className='App'>
+      <Title>Hello there!</Title>
+    </div>
+  );
 }
 
 const Title = styled.h1`
-	font-size: 3.5rem;
-	text-align: center;
-	color: palevioletred;
+  font-size: 3.5rem;
+  text-align: center;
+  color: palevioletred;
 `;
 
 export default App;
