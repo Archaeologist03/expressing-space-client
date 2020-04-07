5a1bcab9843e00e46433097e4012821531d2b07f
Archaeologist03 <omniiknight@hotmail.com>
2020-04-07 02:42:51 +0200

    Added logo and favicon and tests for logo component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import logoImg from '../../assets/images/galaxy-logo.svg';
 
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <LogoContainer>
+      <LogoImage>
+        <img className='image img-image' src={logoImg} alt='Galaxy logo' />
+      </LogoImage>
+    </LogoContainer>
+  );
 };
+
+const LogoContainer = styled.div`
+  display: inline-block;
+  position: absolute;
+  left: 1%;
+  top: 2%;
+`;
+
+const LogoImage = styled.picture`
+  .image {
+    width: 5rem;
+    transition: transform 1s, background-color 1s, opacity 0.5s,
+      border-radius 0.1s;
+    transition-timing-function: ease-out;
+
+    &:hover {
+      transform: rotateZ(-360deg) scale(1);
+      background-color: rgba(255, 0, 191, 0.15);
+      border-radius: 33%;
+    }
+  }
+`;
 
 export default Logo;
