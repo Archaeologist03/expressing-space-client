e83b845ba07c800fec66acb900f69013699c6f68
Archaeologist03 <omniiknight@hotmail.com>
2020-04-19 16:48:52 +0200

    Fixing Like.test by adding mocked props

--- src/components/Likes/Like/Like.test.js
+++ src/components/Likes/Like/Like.test.js
@@ -3,5 +3,14 @@ import { shallow } from 'enzyme';
 import Like from './Like';
 
 it('renders without crashing', () => {
-  shallow(<Like />);
+  const likeTitle = 'Books';
+  const likeData = [
+    { id: 1, name: 'Idiot', author: 'Fyodor Dostoyevsky' },
+    { id: 2, name: '1984', author: 'George Orwell' },
+    { id: 3, name: '1984', author: 'George Orwell' },
+    { id: 4, name: '1984', author: 'George Orwell' },
+    { id: 5, name: '1984', author: 'George Orwell' },
+  ];
+
+  shallow(<Like likeData={likeData}>{likeTitle}</Like>);
 });
